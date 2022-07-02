752f0ff3843850598a69ca1e6f152e9d321b136a
Dilek Karaman <98784815+DilekKaraman@users.noreply.github.com>
2022-07-02 11:57:21 +0100

    json plugin added

--- src/test/java/com/bleucrm/runners/CukesRunner.java
+++ src/test/java/com/bleucrm/runners/CukesRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin={
-                "html=taget/cucumber-report.html"
+                "html=taget/cucumber-report.html",
+                "json:target/cucumber.json"
         },
         features ="src/test/resources/features",
         glue="com/bleucrm/step_definitions",
