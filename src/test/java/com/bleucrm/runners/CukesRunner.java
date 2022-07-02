2cdce385c73d63373cf2d4c937516d0b56f8dccb
Dilek Karaman <98784815+DilekKaraman@users.noreply.github.com>
2022-07-02 11:44:30 +0100

    CukesRunner added

--- src/test/java/com/bleucrm/runners/CukesRunner.java
+++ src/test/java/com/bleucrm/runners/CukesRunner.java
@@ -1,4 +1,20 @@
 package com.bleucrm.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={
+                "html=taget/cucumber-report.html"
+        },
+        features ="src/test/resources/features",
+        glue="com/bleucrm/step_definitions",
+        dryRun=true,
+        tags=""
+
+)
 public class CukesRunner {
 }
